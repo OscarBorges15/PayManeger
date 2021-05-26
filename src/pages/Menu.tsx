cebf7df0bc15339b3aa7e565a99ea57ccdaf04bc
pedroGNF <50996658+pedroGNF@users.noreply.github.com>
2021-05-25 23:30:52 -0300

    Update Menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -18,137 +18,116 @@ import {useNavigation} from '@react-navigation/core';
 import {Button} from '../components/Button';
 import {BtnAdicionar} from '../components/BtnAdicionar';
 import {SkillCard} from '../components/SkillCard';
-import {Header} from '../components/Header';
-
-import {Load} from '../components/Load';
-
-
+import {Header} from '../components/Header'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 interface SkillData {
   id: string;
   name: string;
+  number: number;
 }
 
 export function Menu() {
   const navigation = useNavigation(); 
   const [name, setName] = useState<string>();
 
-  const [newSkill, setNewSkill] = useState('');
-  const [listSkills, setListSkills] = useState<SkillData[]>([]);
-  const [greetings, setGreetings] = useState('');
-
-  const[loading, setLoadinig] = useState(true);
-  const [isVisible, setVisible] = useState(false);
-
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoadinig(false);
-    }, 1200 );
-
-  }, [ ]);
-
-  if(loading){
-    return(
-      <View>
-      <Load/>
-      </View>
-    )
-  }
   async function handleStart(){
     navigation.navigate('EnviarRecibo');
-
   }
 
-  function handleInputChange(value : string){
-    setName(value);
-    
-  }
+  
+
+  const [newSkill, setNewSkill] = useState('');
+  const [newValor, setNewValor] = useState('');
+  const [listSkills, setListSkills] = useState<SkillData[]>([]);
 
 
-  function handleRemoveSkill(id: string) {
-    Alert.alert('Deletar Skill', 'Deseja deletar a skill selecionada?', [
-      {
-        text: 'Não',
-      },
-      {
-        text: 'Sim',
-        onPress: () => {
-          return setListSkills(oldState => oldState.filter(skill => skill.id !== id))
-        }
-      },
-    ])
-  }
+  
 
   function handleAddNewSkills() {
 
+    if(newSkill === '' && newValor === ''){
+      Alert.alert('Atenção ⚠', 'Você não nomeou um produto nem informou o seu valor')
+      return;
+    }
+
     if(newSkill === ''){
-      Alert.alert('Atenção ⚠', 'Você não nomeou nenhum produto')
+      Alert.alert('Atenção ⚠', 'Você não nomeou o produto')
+      return;
+    }
+
+    if(newValor === ''){
+      Alert.alert('Atenção ⚠', 'Você não informou o valor')
       return;
     }
 
     const data = {
       id: String(new Date().getTime()),
-      name: newSkill
+      name: newSkill,
+      number: newValor,
     }
+  
 
     setListSkills(oldState => [...oldState, data]);
 
     setNewSkill('');
+    setNewValor('');
   }
+
+  
 
   return (
     <SafeAreaView style={style.container}>
+
       <View style={style.header}>
         <Header/>
-      </View>
-
+        </View>
 
       <View style={style.insert}>
+
         <TextInput
           style={style.input}
-          placeholder="Novo Produto"
+          placeholder="Nome Produto"
           placeholderTextColor="#555"
           value={newSkill}
           onChangeText={setNewSkill}
         />
+        <TextInput
+          style={style.input}
+          placeholder="Valor Produto"
+          placeholderTextColor="#555"
+          value={newValor}
+          onChangeText={setNewValor}
+        />
+        <BtnAdicionar title="+" onPress={handleAddNewSkills} />
       </View>
- 
-      <View style={style.conteinerList}>
-      <Text style={[style.title, {marginVertical: 30}]}>Meus Produtos</Text>
-      </View> 
 
-      <ScrollView>
-       <View style={style.conteinerList}>
-       
-        <FlatList 
-          data={listSkills}
-          keyExtractor={item => item.id}
-          renderItem={({item}) =>(
-            
-            <SkillCard 
-            onPress={() => handleRemoveSkill(item.id)}
-            skill={item.name} /> 
+      <View style={style.titulo}>
+      <Text style={[style.tituloTexto, {marginVertical: 30}]}>Meus Produtos</Text>
+      </View>
 
-          )}
+      <ScrollView style={style.conteinerList} showsVerticalScrollIndicator = {false}>
+        <View>
+      <FlatList 
+        data={listSkills}
+        keyExtractor={item => item.id}
+        renderItem={({item}) =>(
+          <SkillCard 
+          skill={item.name}
+          valor={item.number}
+          />
+        )}
+      />
+      </View>
 
-          ListEmptyComponent={
-            <Text style={style.empty}>Nenhum produto adicionada 🤔 </Text>
-          } showsVerticalScrollIndicator={false} />
-
-          
-
-            
+      <View style={style.buttonModal}>
+      <Button
+        title={'Criar recibo'}
+        onPress={handleStart}/>
         </View>
 
-        <View  style={style.buttonModal}>
-          <Button
-            title={'Criar'}
-            onPress={handleAddNewSkills}/>
-        </View>
-        </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -157,9 +136,15 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F1B656',
-    
+    paddingHorizontal: 20,
+    paddingTop: 50,
   },
-  title: {
+
+  titulo: {
+   paddingHorizontal: 37,
+  },
+
+  tituloTexto: {
     color: '#fff',
     fontSize: 24,
     fontWeight: 'bold',
@@ -168,63 +153,42 @@ const style = StyleSheet.create({
     color: '#fff',
   },
   input: {
-    width:"100%",
-    height:50,
+    flex: 1,
     backgroundColor: '#1f1e25',
     color: '#fff',
     fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 10,
+    paddingHorizontal: 5,
+    marginLeft: 5,
   },
-  insert: {
-    marginHorizontal:30,
-    alignItems:'center',
-    justifyContent:'center',
-   
-  },
+
   conteinerList:{
     marginHorizontal:30,
     
   },
 
-  conteinerRecibo:{
-    padding:15,
-    flex:1,
-    alignItems:'center',
-
-
+  insert: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 6,
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: "#F2F2F2",
-    borderRadius: 20,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-
-  
   empty: {
     textAlign: 'center',
     color: 'gray',
     fontSize: 18,
     marginTop: 90,
   },
+
    header:{
-    marginHorizontal:30,
-    alignItems:'center'
+    paddingHorizontal:30,
   },
 
   buttonModal:{
-    justifyContent:'center',
+    justifyContent: 'center',
     alignItems:'center',
-    marginVertical:20,
-    
+    marginVertical: 20,
   },
 });
